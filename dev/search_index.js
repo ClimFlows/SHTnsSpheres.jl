a3395070ab2506daf7f96f6267cae196251ca120
Documenter.jl <documenter@juliadocs.github.io>
2024-03-13 13:56:03 +0000

    build based on e40b8e3

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = SHTnsSphere","category":"page"},{"location":"#SHTnsSphere","page":"Home","title":"SHTnsSphere","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for SHTnsSphere.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [SHTnsSphere]","category":"page"}]
+}
