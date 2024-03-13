eb9c13aba70377c4403ec8caf90169f7270d34e0
Documenter.jl <documenter@juliadocs.github.io>
2024-03-13 21:01:56 +0000

    build based on 2c90451

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"","page":"Home","title":"Home","text":"CurrentModule = SHTnsSphere","category":"page"},{"location":"#SHTnsSphere","page":"Home","title":"SHTnsSphere","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for SHTnsSphere.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [SHTnsSphere]","category":"page"}]
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = SHTnsSpheres","category":"page"},{"location":"#SHTnsSphere","page":"Home","title":"SHTnsSphere","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for SHTnsSpheres.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [SHTnsSpheres]","category":"page"}]
 }
