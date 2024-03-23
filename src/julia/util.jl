df23fe22fc9046b034dc75b1e03cdf706f3cf6e0
Thomas Dubos <thomas.dubos@lmd.ipsl.fr>
2024-03-24 00:13:00 +0100

    New API sample_vector

--- src/julia/util.jl
+++ src/julia/util.jl
@@ -39,36 +39,3 @@ writable(x) = copy_input(x)
 writable(x::InOut) = x.data
 copy_input(x) = copy(x)
 copy_input(x::NamedTuple) = map(copy, x)
-
-
-function sample_vector(ulonlat, sph::SHTnsSphere)
-    (; x, y, z, lon, lat) = sph
-    ucolat = @. -getindex(ulonlat(x, y, z, lon, lat), 2)
-    ulon = @. getindex(ulonlat(x, y, z, lon, lat), 1)
-    return (; ucolat, ulon)
-end
-
-#========= Check that synthesis and analysis are inverses =======#
-
-Base.isapprox(a::NT, b::NT) where {NT<:NamedTuple} = all(map(isapprox, a,b))
-Base.copy(t::NamedTuple) = map(copy, t)
-
-## Returns a smooth vector field a∇x+b∇y+c∇z with a,b,c smooth scalar fields
-function velocity(x,y,z,lon,lat)
-    # ∇x = ∇(cosλ cosϕ) = -sinλ, -cosλ sinϕ
-    # ∇y = ∇(sinλ cosϕ) = cosλ, -sinλ sinϕ
-    # ∇z = ∇ sinϕ = 0, cos ϕ
-    a, b, c = (y+2z)^2, (z+2y)^2, (x+2z)^2
-    a, b, c = (0,0,1)
-    ulon = b*cos(lon)-a*sin(lon)
-    ulat = c*cos(lat)-sin(lat)*(a*cos(lon)+b*sin(lon))
-    return ulon, ulat
-end
-
-function test_inv(sph, F=Float64)
-    uv = sample_vector(velocity, sph)
-    spec = analysis_vector!(void, uv, sph)
-    uv2 = synthesis_vector!(void, spec, sph)
-    @test uv.ulon ≈ uv2.ulon
-    @test uv.ucolat ≈ uv2.ucolat
-end
